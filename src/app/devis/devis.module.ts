946770221ede9e85eadac3236228f6c09cbf3794
yvan <https://github.com/Pallies>
2022-07-21 11:19:10 +0200

    creation du composant et du module devis

---
+++ src/app/devis/devis.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreationComponent } from './creation/creation.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DevisModule { }
