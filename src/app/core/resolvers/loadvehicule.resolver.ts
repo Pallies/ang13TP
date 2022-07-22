9725046dc64b0d8ac9f241a54269397d914c95cb
yvan <https://github.com/Pallies>
2022-07-22 09:52:33 +0200

    suppression des imports non utilise

--- src/app/core/resolvers/loadvehicule.resolver.ts
+++ src/app/core/resolvers/loadvehicule.resolver.ts
@@ -1,6 +1,6 @@
 import { Vehicule } from 'src/app/core/models/vehicule';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApiService } from '../services/api.service';
 
@@ -13,7 +13,7 @@ export class LoadvehiculeResolver implements Resolve<Vehicule[]> {
     ) {
     this.apiService.name = 'vehicules';
   }
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Vehicule[]> {
+  resolve(): Observable<Vehicule[]> {
     return this.apiService.getAll();
   }
 }
