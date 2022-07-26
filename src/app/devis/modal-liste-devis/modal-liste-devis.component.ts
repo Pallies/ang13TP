811bb42a9bfecff5b97b207d87d18d39cd561d35
yvan <https://github.com/Pallies>
2022-07-26 18:05:42 +0200

    suppression via modal

--- src/app/devis/modal-liste-devis/modal-liste-devis.component.ts
+++ src/app/devis/modal-liste-devis/modal-liste-devis.component.ts
@@ -6,29 +6,25 @@ import { Devis } from 'src/app/core/models/devis';
   selector: 'car-modal-liste-devis',
   templateUrl: './modal-liste-devis.component.html',
   styleUrls: ['./modal-liste-devis.component.css'],
-  providers: [ModalDevisService],
 })
 export class ModalListeDevisComponent implements OnInit {
   @Input() ref!: string;
-  @Input() devis!: Devis;
   @ViewChild('close', { static: false }) dimiss!: ElementRef;
   constructor(private modalService: ModalDevisService) {}
 
-  ngOnInit(): void {
-    this.devis;
-  }
+  ngOnInit(): void {}
   get id() {
-    return this.devis.id;
+    return this.modalService.devisDeleted.id;
   }
   get quantite() {
-    return this.devis.quantiteTotal;
+    return this.modalService.devisDeleted.quantiteTotal;
   }
   get prixTTC() {
-    return this.devis.prixHT + this.devis.prixHT * 0.2;
+    return this.modalService.devisDeleted.prixHT + this.modalService.devisDeleted.prixHT * 0.2;
   }
   deleteDevis() {
     this.modalService
-      .delete(this.devis)
+      .delete()
       .subscribe((_) => this.dimiss.nativeElement.click());
   }
 }
