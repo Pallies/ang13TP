7efe320567789967fc1765233acb6a9f9aef4144
yvan <https://github.com/Pallies>
2022-07-26 02:04:47 +0200

    url back-end enum

--- src/app/core/resolvers/loaduser.resolver.ts
+++ src/app/core/resolvers/loaduser.resolver.ts
@@ -6,6 +6,7 @@ import {
   ActivatedRouteSnapshot,
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { URL_BACK } from '../guards/url-back.routes';
 import { Utilisateur } from '../models/utilisateur';
 import { ApiService } from '../services/api.service';
 
@@ -13,11 +14,11 @@ import { ApiService } from '../services/api.service';
   providedIn: 'root',
 })
 export class LoaduserResolver implements Resolve<Utilisateur[]> {
-  
+
   constructor(private apiservice: ApiService<Utilisateur>) {}
 
   resolve(): Observable<Utilisateur[]> {
-    this.apiservice.name = 'utilisateurs';
+    this.apiservice.name = URL_BACK.UTILISATEUR;
     return this.apiservice.getAll();
   }
 }
