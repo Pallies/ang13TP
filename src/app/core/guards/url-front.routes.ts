24f79337abe9e7b5f7bed52d7ae22c059d5df1f2
yvan <https://github.com/Pallies>
2022-07-26 14:44:56 +0200

    gestion menu

--- src/app/core/guards/url-front.routes.ts
+++ src/app/core/guards/url-front.routes.ts
@@ -1,25 +1,49 @@
-
 import { Profil } from '../models/profil';
-export enum RoutesProfil{
-  MENU='/menu',
-  MENU_CLIENT='/menu/clients',
-  MENU_STOCK='',
-  MENU_ENTRETIEN='',
-  MENU_VEHICULE='/menu/vehicules',
-  MENU_TACHE='',
-  MENU_UTILISATEUR='/menu/utilisateurs',
-  MENU_DEVIS='/menu/clients'
+export enum RoutesProfil {
+  MENU = '/menu',
+  MENU_CLIENT = '/menu/clients',
+  MENU_STOCK = '',
+  MENU_ENTRETIEN = '',
+  MENU_VEHICULE = '/menu/vehicules',
+  MENU_TACHE = '',
+  MENU_UTILISATEUR = '/menu/utilisateurs',
+  MENU_DEVIS = '/menu/clients',
 }
-export enum URL_SNAPSHOT{
-  DATA_UTILISATEUR="dataUtilisateurs",
-  DATA_VEHICULE="dataVehicules",
-  DATA_CLIENT="dataClients"
+export enum URL_SNAPSHOT {
+  DATA_UTILISATEUR = 'dataUtilisateurs',
+  DATA_VEHICULE = 'dataVehicules',
+  DATA_CLIENT = 'dataClients',
 }
-export const gestionProfil= {
-[Profil.ADMIN]:[RoutesProfil.MENU,RoutesProfil.MENU_CLIENT,RoutesProfil.MENU_UTILISATEUR,RoutesProfil.MENU_VEHICULE],
-[Profil.CHEF]:[RoutesProfil.MENU,RoutesProfil.MENU_CLIENT,RoutesProfil.MENU_VEHICULE,],
-[Profil.MAG]:[RoutesProfil.MENU,],
-[Profil.MECA]:[RoutesProfil.MENU,],
-[Profil.COM]:[RoutesProfil.MENU,RoutesProfil.MENU_CLIENT,RoutesProfil.MENU_VEHICULE],
-}
-
+export const gestionProfil = {
+  [Profil.ADMIN]: [
+    RoutesProfil.MENU,
+    RoutesProfil.MENU_CLIENT,
+    RoutesProfil.MENU_STOCK,
+    RoutesProfil.MENU_ENTRETIEN,
+    RoutesProfil.MENU_VEHICULE,
+    RoutesProfil.MENU_TACHE,
+    RoutesProfil.MENU_UTILISATEUR
+  ],
+  [Profil.CHEF]: [
+    RoutesProfil.MENU,
+    RoutesProfil.MENU_CLIENT,
+    RoutesProfil.MENU_STOCK,
+    RoutesProfil.MENU_ENTRETIEN,
+    RoutesProfil.MENU_TACHE
+  ],
+  [Profil.MAG]: [
+    RoutesProfil.MENU,
+    RoutesProfil.MENU_STOCK,
+    RoutesProfil.MENU_ENTRETIEN,
+    RoutesProfil.MENU_VEHICULE,
+  ],
+  [Profil.MECA]: [
+     RoutesProfil.MENU,
+    RoutesProfil.MENU_ENTRETIEN,
+    RoutesProfil.MENU_TACHE,
+  ],
+  [Profil.COM]: [
+    RoutesProfil.MENU_CLIENT,
+    RoutesProfil.MENU_VEHICULE,
+  ],
+};
