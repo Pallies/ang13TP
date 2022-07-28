541adb934e59c050d7540d048a751ff49bb72a7b
yvan <https://github.com/Pallies>
2022-07-28 18:02:47 +0200

    cypress menu profil

--- cypress/e2e/menu-page/menu-page.spec.cy.ts
+++ cypress/e2e/menu-page/menu-page.spec.cy.ts
@@ -3,18 +3,20 @@ import { buttonContains, login } from "../fonctions/fonctions";
 describe('menu pour les différents profils', function () {
   this.beforeEach(function(){
     cy.visit('/');
+    cy.url().wait(1000).should('include', 'login');
+    cy.fixture('utilisateurs.json').then((data) => data.utilisateurs).as('utilisateur')
   })
   it('menu Commercial', () => {
     login('COM')
-    cy.url().should('include', 'menu');
-    cy.get('button').should('have.length',2)
+    cy.url().wait(1000).should('include', 'menu');
+    cy.get('button').should('have.length',3)
     cy.get('button').then(($button)=>{
       buttonContains($button,'COM')
     })
   });
   it('menu Mecanicien', () => {
     login('MECA')
-    cy.url().should('include', 'menu');
+    cy.url().wait(1000).should('include', 'menu');
     cy.get('button').should('have.length',2)
     cy.get('button').then(($button)=>{
       buttonContains($button,'MECA')
@@ -22,24 +24,24 @@ describe('menu pour les différents profils', function () {
   });
   it('menu Chef d atelier', () => {
     login('CHEF')
-    cy.url().should('include', 'menu');
-    cy.get('button').should('have.length',4)
+    cy.url().wait(1000).should('include', 'menu');
+    cy.get('button').should('have.length',5)
     cy.get('button').then(($button)=>{
       buttonContains($button,'CHEF')
     })
   });
   it('menu Magasinier', () => {
     login('MAG')
-    cy.url().should('include', 'menu');
-    cy.get('button').should('have.length',3)
+    cy.url().wait(1000).should('include', 'menu');
+    cy.get('button').should('have.length',4)
     cy.get('button').then(($button)=>{
       buttonContains($button,'MAG')
     })
   });
   it('menu Administrateur', () => {
     login('ADMIN')
-    cy.url().should('include', 'menu');
-    cy.get('button').should('have.length',6)
+    cy.url().wait(1000).should('include', 'menu');
+    cy.get('button').should('have.length',7)
     cy.get('button').then(($button)=>{
       buttonContains($button,'ADMIN')
     })
